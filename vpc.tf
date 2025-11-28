e38a5a4179d57e38e8470bd441d8ee65afc8ffa5
Nagendrakeerthipati <keerthipatinagendra4@gmail.com>
2025-11-28 16:15:44 +0530

    VPc Module

---
+++ vpc.tf
@@ -0,0 +1,13 @@
+# roboshop-dev
+resource "aws_vpc" "main" {
+  cidr_block           = var.cidr_block
+  instance_tenancy     = "default"
+  enable_dns_hostnames = "true"
+
+  tags = merge(
+    local.common_tags,
+    {
+      Name : "${var.project}-${var.environment}"
+    }
+  )
+}
