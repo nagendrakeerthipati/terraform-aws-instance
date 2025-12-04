5e844d6b2d5c0e375af73b44a81cc4624b889770
Nagendrakeerthipati <keerthipatinagendra4@gmail.com>
2025-12-04 18:27:13 +0530

    Updated main.tf and added variables.tf

--- variables.tf
+++ variables.tf
@@ -12,9 +12,72 @@ variable "cidr_block" {
   default = "10.0.0.0/16"
 
 }
+variable "IGW_tags" {
+  type    = map(string)
+  default = {}
+}
 
 variable "public_subnet_cidr" {
   type = list(string)
 
+}
+
+variable "public_subnet_cidr_tags" {
+  type    = map(string)
+  default = {}
+}
+
+
+variable "private_subnet_cidr" {
+  type = list(string)
 
 }
+
+
+variable "private_subnet_cidr_tags" {
+  type    = map(string)
+  default = {}
+}
+
+variable "database_subnet_cidr" {
+  type = list(string)
+
+}
+variable "database_subnet_cidr_tags" {
+  type    = map(string)
+  default = {}
+}
+
+variable "eip_tags" {
+  type    = map(string)
+  default = {}
+
+}
+
+
+variable "nat_gateway_tags" {
+  type    = map(string)
+  default = {}
+
+}
+
+
+variable "public_route_tags" {
+  type    = map(string)
+  default = {}
+
+}
+
+
+variable "private_route_tags" {
+  type    = map(string)
+  default = {}
+
+}
+
+variable "database_route_tags" {
+  type    = map(string)
+  default = {}
+
+}
+
--- vpc.tf
+++ vpc.tf
@@ -14,10 +14,11 @@ resource "aws_vpc" "main" {
 }
 
 # IGW roboshop-dev
-resource "aws_internet_gateway" "main" {
+resource "aws_internet_gateway" "IGW" {
   vpc_id = aws_vpc.main.id
 
   tags = merge(
+    var.IGW_tags,
     local.common_tags,
     {
       Name = "${var.project}-${var.environment}"
@@ -37,6 +38,7 @@ resource "aws_subnet" "public" {
 
 
   tags = merge(
+    var.public_subnet_cidr_tags,
     local.common_tags,
     {
       Name = "${var.project}-${var.environment}-public-${local.az_names[count.index]}"
@@ -44,4 +46,151 @@ resource "aws_subnet" "public" {
   )
 
 
+}
+
+resource "aws_subnet" "private" {
+  count             = length(var.private_subnet_cidr)
+  vpc_id            = aws_vpc.main.id
+  cidr_block        = var.private_subnet_cidr[count.index]
+  availability_zone = local.az_names[count.index]
+
+
+
+  tags = merge(
+    var.private_subnet_cidr_tags,
+    local.common_tags,
+    {
+      Name = "${var.project}-${var.environment}-private-${local.az_names[count.index]}"
+    }
+  )
+
+
+}
+
+
+resource "aws_subnet" "database" {
+  count             = length(var.database_subnet_cidr)
+  vpc_id            = aws_vpc.main.id
+  cidr_block        = var.database_subnet_cidr[count.index]
+  availability_zone = local.az_names[count.index]
+
+
+
+  tags = merge(
+    var.database_subnet_cidr_tags,
+    local.common_tags,
+    {
+      Name = "${var.project}-${var.environment}-database-${local.az_names[count.index]}"
+    }
+  )
+
+
+}
+
+resource "aws_eip" "nat" {
+  domain = "vpc"
+
+  tags = merge(
+    var.eip_tags,
+    local.common_tags,
+    {
+      Name = "${var.project}-${var.environment}"
+    }
+  )
+}
+resource "aws_nat_gateway" "nat_gateway" {
+  allocation_id = aws_eip.nat.id
+  subnet_id     = aws_subnet.public[0].id # Use first public subnet(since there are 2 public subnets )
+
+  tags = merge(
+    var.nat_gateway_tags,
+    local.common_tags,
+    {
+      Name = "${var.project}-${var.environment}"
+    }
+
+  )
+
+  # To ensure proper ordering, it is recommended to add an explicit dependency
+  # on the Internet Gateway for the VPC.
+  depends_on = [aws_internet_gateway.IGW]
+}
+
+
+resource "aws_route_table" "public" {
+  vpc_id = aws_vpc.main.id
+  tags = merge(
+    var.public_route_tags,
+    local.common_tags,
+
+    {
+      Name = "${var.project}-${var.environment}-public"
+    }
+
+  )
+}
+
+resource "aws_route_table" "private" {
+  vpc_id = aws_vpc.main.id
+  tags = merge(
+    var.public_route_tags,
+    local.common_tags,
+
+    {
+      Name = "${var.project}-${var.environment}-private"
+    }
+
+  )
+}
+
+resource "aws_route_table" "database" {
+  vpc_id = aws_vpc.main.id
+  tags = merge(
+    var.public_route_tags,
+    local.common_tags,
+
+    {
+      Name = "${var.project}-${var.environment}-database"
+    }
+
+  )
+}
+
+
+resource "aws_route" "public" {                           # Creating a route inside a route table
+  route_table_id         = aws_route_table.public.id      # Which route table to update
+  destination_cidr_block = "0.0.0.0/0"                    # Destination: all internet traffic
+  nat_gateway_id         = aws_nat_gateway.nat_gateway.id # Send traffic through NAT Gateway
+}
+
+resource "aws_route" "private" {                          # Creating a route inside a route table
+  route_table_id         = aws_route_table.private.id     # Which route table to update
+  destination_cidr_block = "0.0.0.0/0"                    # Destination: all internet traffic
+  nat_gateway_id         = aws_nat_gateway.nat_gateway.id # Send traffic through NAT Gateway
+}
+
+
+resource "aws_route" "database" {                         # Creating a route inside a route table
+  route_table_id         = aws_route_table.database.id    # Which route table to update
+  destination_cidr_block = "0.0.0.0/0"                    # Destination: all internet traffic
+  nat_gateway_id         = aws_nat_gateway.nat_gateway.id # Send traffic through NAT Gateway
+}
+
+
+resource "aws_route_table_association" "public" {
+  count          = length(var.public_subnet_cidr)
+  subnet_id      = aws_subnet.public[count.index].id
+  route_table_id = aws_route_table.public.id
+}
+
+resource "aws_route_table_association" "private" {
+  count          = length(var.private_subnet_cidr)
+  subnet_id      = aws_subnet.private[count.index].id
+  route_table_id = aws_route_table.private.id
+}
+
+resource "aws_route_table_association" "database" {
+  count          = length(var.database_subnet_cidr)
+  subnet_id      = aws_subnet.database[count.index].id
+  route_table_id = aws_route_table.database.id
 }
