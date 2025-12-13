05567984f88e884a60860b57f63ef759b24bee63
Nagendrakeerthipati <keerthipatinagendra4@gmail.com>
2025-12-13 09:12:41 +0530

    Updated main.tf and added variables.tf

--- locals.tf
+++ locals.tf
@@ -1,12 +1,3 @@
-# locals {
-#   common_tags = {
-#     Project     = var.project
-#     Environment = var.environment
-#     terraform   = "true"
-#   }
-#   az_names = slice(data.aws_availability_zones.available.names, 0, 2)
-# }
-
 
 locals {
   common_tags = {
