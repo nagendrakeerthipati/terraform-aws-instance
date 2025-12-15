6730cc5f914f0ab9259779e290a78181707fab96
Nagendrakeerthipati <keerthipatinagendra4@gmail.com>
2025-12-15 20:27:28 +0530

    10-sg

--- outputs.tf
+++ outputs.tf
@@ -6,7 +6,7 @@ output "vpc_id" {
 }
 
 output "public_subnet_ids" {
-  value = aws_subnet.public[*].id
+  value = aws_subnet.public[*].id # "*" will return list of all ids in the public subnets
 }
 
 output "private_subnet_ids" {
